dc3ec3860fb546dd5830f8fdb3909b7b3bf47abf
Sorecchione07435 <Sorecchione07@gmail.com>
2026-05-07 20:15:03 +0200

    update setup.py

--- setup.py
+++ setup.py
@@ -12,6 +12,7 @@ setup(
     long_description_content_type="text/markdown",
     url="https://github.com/Sorecchione07435/DeployStack",
     packages=find_packages(),
+    include_package_data=True,
     install_requires=[
         "psutil",
         "python-dotenv",
